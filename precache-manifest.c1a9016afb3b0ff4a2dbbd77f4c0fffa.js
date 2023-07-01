debf2148b7530c38ab279ddc846f1019574459b8
Sayantan <sayantanpal2002@gmail.com>
2023-07-01 12:32:51 +0530

    Updates

---
+++ precache-manifest.c1a9016afb3b0ff4a2dbbd77f4c0fffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a304cfb414edab94aff20c65cd3db65f",
+    "url": "/crypto-tracker/index.html"
+  },
+  {
+    "revision": "5e69a103b586eac1891b",
+    "url": "/crypto-tracker/static/css/main.c16d525c.chunk.css"
+  },
+  {
+    "revision": "2f3e95c045ba9afc2f0f",
+    "url": "/crypto-tracker/static/js/2.1062d4b7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/crypto-tracker/static/js/2.1062d4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e69a103b586eac1891b",
+    "url": "/crypto-tracker/static/js/main.2c40bfd7.chunk.js"
+  },
+  {
+    "revision": "3046e15a071d6251821d",
+    "url": "/crypto-tracker/static/js/runtime-main.50d108b2.js"
+  }
+]);
